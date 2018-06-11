7501f5b4c1a29bc9306168eccb6aefb7218fd2d1
sravanthiarjun <38848713+sravanthiarjun@users.noreply.github.com>
2018-06-11 17:05:14 +0530

    Update log1.py

--- log1.py
+++ log1.py
@@ -31,11 +31,11 @@ def error_percentage():
 cur.execute("""select to_char(day, 'Mon DD, YYYY') as day, percentage from
             "errors_percent where percentage > 1.0 group by day, percentage
             "order by"percentage desc;""")
-    result = cur.fetchall()
-    for i in range(len(result)):
-        date = result[i][0]
-        errors_percent = result[i][1]
-        print("%s--%.1f %%" %(date, errors_percent))
+result = cur.fetchall()
+for i in range(len(result)):
+    date = result[i][0]
+    errors_percent = result[i][1]
+    print("%s--%.1f %% " % (date, errors_percent))
 popular_articles()
 popular_authors()
 error_percentage()
